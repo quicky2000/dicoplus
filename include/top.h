3b751ee0e8dd9d01de4acdde0b7b17696dcb1626
Julien Thevenon <julien_thevenon@yahoo.fr>
2014-02-21 18:07:49 +0100

    Move GUI in top and relate cell synoptic representation to systemC cells

--- include/top.h
+++ include/top.h
@@ -3,28 +3,64 @@
 
 #include "systemc.h"
 #include "dicoplus.h"
+#include "synoptic.h"
+#include "dicoplus_synoptic_grid.h"
+#include "screen_display.h"
 
 namespace dicoplus
 {
   class top:public sc_module
   {
   public:
+    SC_HAS_PROCESS(top);
     inline top(sc_module_name name,
                const std::string & p_config_file_name);
   private:
+    inline void refresh_GUI(void);
     sc_clock m_clk;
     dicoplus m_dicoplus;
+    synoptic::synoptic m_synoptic;
+    dicoplus_synoptic_grid m_grid;
   };
 
   //------------------------------------------------------------------------------
   top::top(sc_module_name name,
-         const std::string & p_config_file_name):
-  sc_module(name),
-  m_clk("clk",10,SC_NS,05),
-  m_dicoplus("dicoplus",p_config_file_name)    
+	   const std::string & p_config_file_name):
+    sc_module(name),
+    m_clk("clk",10,SC_NS,05),
+    m_dicoplus("dicoplus",p_config_file_name),
+    m_synoptic(1550,850),
+    m_grid((dicoplus_synoptic_cell::compute_dim(),m_synoptic),"grid",m_dicoplus.get_width(),m_dicoplus.get_height())
     {
+      // Bind clock
       m_dicoplus.m_clk(m_clk);
+
+      // Manage GUI
+      for(uint32_t l_x_index = 0 ; l_x_index < m_dicoplus.get_width() ; ++l_x_index)
+	{
+	  for(uint32_t l_y_index = 0 ; l_y_index < m_dicoplus.get_height() ; ++l_y_index)
+	    {
+	      m_dicoplus.attach_cell_listener(l_x_index,l_y_index,m_grid.get_cell(l_x_index,l_y_index));
+	    }
+	}
+
+      m_synoptic.add_zone(642,0,m_grid);
+      screen_display l_screen_display(m_synoptic);
+      m_synoptic.add_zone(0,0,l_screen_display);
+      m_synoptic.pack();
+      m_synoptic.paint();
+      m_synoptic.refresh();
+
+      SC_METHOD(refresh_GUI);
+      sensitive << m_clk.negedge_event();
     }
+
+    //------------------------------------------------------------------------------
+    void top::refresh_GUI(void)
+    {
+      m_synoptic.refresh();
+    }
+    
 }
 #endif // _TOP_H_
 //EOF
